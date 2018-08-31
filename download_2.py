97d66d9b2ed0f721edc326d57e3d3b17d8a3db07
Regan Kouchoo <KOUC01@stpiusx.nsw.edu.au>
2018-08-31 12:20:15 +1000

    worked on new script

--- download_2.py
+++ download_2.py
@@ -5,8 +5,7 @@ from anime_downloader import get_anime_class
 
 # Class refrence: https://github.com/vn-ki/anime-downloader/wiki/Package-documentation
 
-targetSeriesURL = argv[1] # the target series to index and download
-#targetDirectory = argv[2] # the target path to download  
+targetSeriesURL = argv[1]  # the target series to index and download
 
 print("Getting video meta-data for the url: {}".format(targetSeriesURL))
 kissAnimeInterface = get_anime_class('kissanime')
@@ -19,8 +18,32 @@ print("Downloading {} episdoes. This may take some time!".format(episodes))
 
 # for every episode we want to download it
 for i in range(0, episodes):
-	print("Downloading episode ({} of {}). Name: {}".format(currentAnime[i].pretty_title, episodes, currentAnime.title))
-	currentAnime[i].download() # download the episode to the current drectory.
+    print("Downloading episode ({} of {}). Name: {}".format(currentAnime[i].ep_no, episodes, currentAnime.pretty_title))
+    currentAnime[i].download()  # download the episode to the current drectory.
 
-def moveVideosToSelectedDir():
-	
+
+def getVideoFileList(dir):
+    path, dirs, files = next(os.walk(dir))
+    videoFiles = []
+
+    for file in files:
+        if ".mp4" in file:
+            videoFiles.append(file)
+
+    return videoFiles
+
+
+def moveVideosToSelectedDir(dir):
+    files = getVideoFileList("~")
+
+    try:
+        os.stat(dir)
+    except:
+        os.mkdir(dir)
+
+
+for file in files:
+    print("Moving {} to {}".format(file, dir + "/" + file))
+    os.rename(file, dir + "/" + file)  # try and move the file to the directory
+
+moveVideosToSelectedDir(currentAnimeName)
