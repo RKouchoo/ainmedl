d3790642cbd86fc510f12dea9ae160e09d0ec107
RKouchoo <r.kouchoo1@gmail.com>
2018-09-03 13:03:32 +1000

    new retry list method, prep for yaml saves

--- download_2.py
+++ download_2.py
@@ -1,6 +1,6 @@
 import os
+import yaml
 from sys import argv
-
 from anime_downloader.sites import get_anime_class
 
 # Class refrence: https://github.com/vn-ki/anime-downloader/wiki/Package-documentation
@@ -12,8 +12,8 @@ def getVideoFileList(dir):
     videoFiles = []
 
     for file in files:
-    	if ".mp4" in file:
-    		videoFiles.append(file)
+        if ".mp4" in file:
+            videoFiles.append(file)
 
     return videoFiles
 
@@ -26,8 +26,8 @@ def moveVideosToSelectedDir(dir):
         os.mkdir(dir)
 
     for file in files:
-    	print("Moving {} to {}".format(file, dir + "/" + file))
-    	os.rename(file, dir + "/" + file)  # try and move the file to the directory
+        print("Moving {} to {}".format(file, dir + "/" + file))
+        os.rename(file, dir + "/" + file)  # try and move the file to the directory
 
 
 def getDownloadFromUrl(url):
@@ -39,14 +39,30 @@ def getDownloadFromUrl(url):
     episodes = len(currentAnime)
     print("Found {} videos in this series to download. \n".format(episodes))
     print("Downloading {} episdoes. This may take some time!".format(episodes))
+    redownload = []
 
     # for every episode we want to download it
     for i in range(0, episodes):
-        print("Downloading episode ({} of {}). Name: {}".format(currentAnime[i].ep_no, episodes, currentAnime[i].pretty_title))
-        currentAnime[i].download()  # download the episode to the current drectory.
-	
-	# once the files have been downloaded move them to the correct directory
+        title = currentAnime[i].pretty_title
+        print("Downloading episode ({} of {}). Name: {}".format(currentAnime[i].ep_no, episodes, title))
+        try:
+            currentAnime[i].download()  # download the episode to the current drectory.
+        except:
+            print("Encountered an error downloading the video: {}. Adding {} to be re-downloaded".format(title, title))
+            redownload.append(i)
+
+        # redownload failed videos. If they fail again then they aren't going to be dowloaded
+        # might implement a persistent loop  
+        for x in redownload:
+            print("[RETRY]: Downloading episode ({} of {}). Name: {}".format(currentAnime[x].ep_no, episodes, currentAnime[x].pretty_title))
+            try:
+                currentAnime[x].download()
+            except:
+                print("Failed to download {}. Please download it manually!".format(currentAnime[x].pretty_title))
+
+        redownload = []
+
+    # once the files have been downloaded move them to the correct directory
     moveVideosToSelectedDir(currentAnimeName)
 
 getDownloadFromUrl(targetSeriesURL)
-	
